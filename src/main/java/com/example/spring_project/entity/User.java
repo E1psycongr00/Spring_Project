1f4e59531400d48e6485be80ebbca0612fb7274a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-01 02:09:20 +0900

    feat(UserEntity): Entity 추가
    User 엔티티 속성
    
    - msrl id (pk)
    - uid String (uk, not null)
    - name String
    (not null)

---
+++ src/main/java/com/example/spring_project/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.spring_project.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // 접근자를 private으로 설정
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long msrl;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String uid;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+
+}
