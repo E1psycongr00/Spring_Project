1a736d4e6c09c323e0872bf8994d41398b451d28
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-04 10:39:22 +0900

    feat(UserController): 기능 및 테스트 추가
    UserController를 Service를 이용해 response를 리턴하도록 바꿈
    메서드 mapping이 잘 동작하는 지만
    테스트 수행함.

--- src/main/java/com/example/spring_project/controller/UserController.java
+++ src/main/java/com/example/spring_project/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.example.spring_project.controller;
 
-import com.example.spring_project.entity.User;
-import com.example.spring_project.repository.UserRepository;
+import com.example.spring_project.dto.UserDto;
+import com.example.spring_project.dto.response.Response;
+import com.example.spring_project.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Api(tags ={"1. User"}) // UserController 최상단 타이틀 영역에 표시될 값
 // class 상단에 선언하면 내부의 final 객체에 대하여 Constructor Injection을 수행한다.
@@ -17,20 +16,40 @@ import java.util.List;
 @RestController         // 결과값은 JSON으로 출력 @ResponseBody를 사용 할 필요가 없음.
 @RequestMapping(value = "/v1") // api resource를 버전별로 관리하기 위해서 /v1 이 모두 적용되게 처리함.
 public class UserController {
-    private final UserRepository userRepository; // @RequiredArgsConstructor 에 의해 자동으로 주입됨.
+    private final UserService userService; // @RequiredArgsConstructor 에 의해 자동으로 주입됨.
 
     @ApiOperation(value = "회원조회", notes = "모든 회원을 조회한다.") // 각각의 resource에 설명 제공
     @GetMapping(value = "/users")  // 파라미터에 대한 설명을 보여주기 위해 세팅
-    public List<User> findAllUser() {
-        return userRepository.findAll();
+    public Response<UserDto> findAllUser() {
+        Response<UserDto> response = userService.findAll();
+        return response;
+    }
+
+    @ApiOperation(value = "회원조회", notes = "이름을 통해 모든 회원을 조회한다.")
+    @GetMapping(value = "/users/{name}")
+    public Response<UserDto> findAllUserByUserid(@PathVariable String name) {
+        Response<UserDto> response = userService.findByUserid(name);
+        return response;
     }
 
     @ApiOperation(value="회원 입력", notes = "회원을 입력한다.")
-
     @PostMapping(value = "/user")
-    public User Save(@ApiParam(value = "회원 아이디 정보", required = true) @RequestBody User user) {
-        return userRepository.save(user);
+    public Response<UserDto> saveUser( @RequestBody UserDto userDto) {
+        Response<UserDto> response = userService.insertUser(userDto);
+        return response;
     }
 
+    @ApiOperation(value="회원 수정", notes = "회원을 수정한다.")
+    @PutMapping(value = "/user")
+    public Response<UserDto> updateUser(@RequestBody UserDto userDto) {
+        Response<UserDto> response = userService.updateUser(userDto);
+        return response;
+    }
 
+    @ApiOperation(value="회원 삭제", notes = "회원을 삭제한다.")
+    @PutMapping(value = "/user/{userid}")
+    public Response<UserDto> updateUser(@PathVariable String userid) {
+        Response<UserDto> response = userService.deleteUser(userid);
+        return response;
+    }
 }
